6c8689b082bd61e99b1778ce5b135f9bf238a996
Martina Babinska <mb@web-solutions.sk>
2017-04-11 02:29:14 +0200

    settings for Entity, DataFixtires, DoctrineMigrations

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class DefaultController
+ *
+ * @package AppBundle\Controller
+ */
+class DefaultController extends Controller
+{
+    /**
+     * @Template()
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
---
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * Class DefaultControllerTest
+ *
+ * @package AppBundle\Tests\Controller
+ */
+class DefaultControllerTest extends WebTestCase
+{
+    /**
+     *
+     */
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('Hello World', $client->getResponse()->getContent());
+    }
+}
