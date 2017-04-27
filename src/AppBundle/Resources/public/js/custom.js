beb8f46d0ce4a49a982f91d7086af0929c33108e
Martina Babinska <mb@web-solutions.sk>
2017-04-28 01:53:02 +0200

    Top, Left, Right navigation - responsivity

--- src/AppBundle/Resources/public/js/custom.js
+++ src/AppBundle/Resources/public/js/custom.js
@@ -3,3 +3,18 @@ function toggleAnswer(param) {
     console.log($answerId);
     $($answerId).toggle(1000);
 }
+
+/*-----------------------------------------------------------------------------------*/
+/*	LEFT + TOP MENU
+ /*-----------------------------------------------------------------------------------*/
+jQuery(document).ready(function ($) {
+    var $leftMenuTrigger = $('#cd-menu-trigger'),
+        $topMenu = $('#ownmenu'),
+        $rightNavigation = $('#choose_color');
+
+    //hide leftMenuNavigation and rightNavigation if topMenu is opened
+    $topMenu.on('click', function (event) {
+        $leftMenuTrigger.toggleClass('hide-element');
+        $rightNavigation.toggleClass('hide-element');
+    });
+});
