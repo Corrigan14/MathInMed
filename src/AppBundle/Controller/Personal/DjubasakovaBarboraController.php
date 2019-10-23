35d81ee37b59ffab995b2e01d3da33be9fb875ae
Corrigan14 <babinskam@aol.com>
2019-10-23 13:38:10 +0200

    Structure of a profile wbpage

---
+++ src/AppBundle/Controller/Personal/DjubasakovaBarboraController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller\Personal;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class MainController
+ * @package AppBundle\Controller\Personal
+ */
+class DjubasakovaBarboraController extends Controller
+{
+    /**
+     * @Template()
+     *
+     * @param string|boolean $locale
+     * @return array
+     */
+    public function introductionAction($locale = false): array
+    {
+        return [];
+    }
+}
